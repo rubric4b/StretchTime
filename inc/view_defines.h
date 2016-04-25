ba161cef40fc0f76dc75488e79cb019904114e9a
Grayish <hobbangz5@gmail.com>
2016-04-26 00:24:06 +0900

    add header file for edje

---
+++ inc/view_defines.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#if !defined(_VIEW_DEFINES_H)
+#define _VIEW_DEFINES_H
+
+#define PART_BACKGROUND "background"
+#define PART_HAND_HOUR "hand_hour"
+#define PART_HAND_MINUTE "hand_minute"
+
+#define MSG_ID_SET_TIME 1
+#define MSG_ID_AMBIENT_MODE 2
+
+
+#endif
